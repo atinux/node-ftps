85be5b73e443d4d29fe8411791f3dad771f8e224
Ruben Stolk <ruben.stolk@capptions.com>
2022-01-31 11:45:31 +0100

    Add bareCredentials as option to overcome username/password difficulties

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ var dcp = require('duplex-child-process')
  ** {
  **   host: 'domain.com', // Required
  **   username: 'Test', // Optional. Use empty username for anonymous access.
- **   password: 'Test', // Required if username is not empty, except when requiresPassword: false
+ **   password: 'Test', // Required if username is not empty, except when requiresPassword: false,
+ **   bareCredentials: 'Test,Test$T', // Used instead of username/password for special cases where passwords should not be escaped
  **   protocol: 'sftp', // Optional, values : 'ftp', 'sftp', 'ftps', ... default: 'ftp'
  **   // protocol is added on beginning of host, ex : sftp://domain.com in this case
  **   port: '22', // Optional
@@ -43,6 +44,7 @@ FTP.prototype.initialize = function (options) {
     protocol: 'ftp',
     username: '',
     password: '',
+    bareCredentials: '',
     escape: true,
     retries: 1, // LFTP by default tries an unlimited amount of times so we change that here
     timeout: 10, // Time before failing a connection attempt
@@ -58,11 +60,11 @@ FTP.prototype.initialize = function (options) {
   }
 
   // Extend options with defaults
-  var opts = _.pick(_.extend(defaults, options), 'host', 'username', 'password', 'port', 'escape', 'retries', 'timeout', 'retryInterval', 'retryIntervalMultiplier', 'requiresPassword', 'protocol', 'autoConfirm', 'cwd', 'additionalLftpCommands', 'requireSSHKey', 'sshKeyPath', 'sshKeyOptions')
+  var opts = _.pick(_.extend(defaults, options), 'host', 'username', 'password', 'bareCredentials', 'port', 'escape', 'retries', 'timeout', 'retryInterval', 'retryIntervalMultiplier', 'requiresPassword', 'protocol', 'autoConfirm', 'cwd', 'additionalLftpCommands', 'requireSSHKey', 'sshKeyPath', 'sshKeyOptions')
 
   // Validation
   if (!opts.host) throw new Error('You need to set a host.')
-  if (opts.username && opts.requiresPassword === true && !opts.password) throw new Error('You need to set a password.')
+  if (opts.username && opts.requiresPassword === true && !opts.password && !opts.bareCredentials) throw new Error('You need to set a password.')
   if (opts.protocol && typeof opts.protocol !== 'string') throw new Error('Protocol needs to be of type string.')
   if (opts.requireSSHKey === true && !opts.sshKeyPath) throw new Error('You need to set a ssh key path.');
 
@@ -116,7 +118,10 @@ FTP.prototype.prepareLFTPOptions = function () {
   opts.push(this.options.additionalLftpCommands)
 
   var open = 'open'
-  if (this.options.username) {
+  if (this.options.bareCredentials) {
+    open += ' -u \'' + this.options.bareCredentials + '\''
+  }
+  else if (this.options.username) {
     open += ' -u "' + this._escapeshell(this.options.username) + '","' + this._escapeshell(this.options.password) + '"'
   }
   open += ' "' + this.options.host + '"'
